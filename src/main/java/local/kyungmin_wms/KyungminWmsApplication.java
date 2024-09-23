cee3dd17af8acd1ae019c4e776135c5107f53938
ceo92 <king9243@naver.com>
2024-09-23 16:01:12 +0900

    chore: 초기 프로젝트 생성

---
+++ src/main/java/local/kyungmin_wms/KyungminWmsApplication.java
@@ -0,0 +1,14 @@
+package local.kyungmin_wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class KyungminWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyungminWmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/kyungmin_wms/KyungminWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package local.kyungmin_wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyungminWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
