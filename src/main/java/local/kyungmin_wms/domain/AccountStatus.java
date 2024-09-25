ed7faf34112ae90284ce5e3f785a7ed516739f36
ceo92 <king9243@naver.com>
2024-09-26 02:08:02 +0900

    feat: AccountStatus(계정 상태 enum) 생성

--- src/main/java/local/kyungmin_wms/domain/AccountStatus.java
+++ src/main/java/local/kyungmin_wms/domain/AccountStatus.java
@@ -2,8 +2,7 @@ package local.kyungmin_wms.domain;
 
 public enum AccountStatus {
 
-
-  ("승인 대기") , EXPLOSIVE("활성") , BIOLOGICAL("탈퇴 대기") , NORMAL("비활성");
+  WAITING_APPROVAL("승인 대기") , ACTIVATION("활성") , WAITING_CANCEL("탈퇴 대기") , INACTIVATION("비활성");
   private final String description;
 
   AccountStatus(String description){
