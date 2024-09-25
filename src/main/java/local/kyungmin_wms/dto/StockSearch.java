985bd5cf619b2b0308ec2c64ff345aaf09b9a0e6
ceo92 <king9243@naver.com>
2024-09-25 17:50:35 +0900

    feat: 재고 검색 조건 dto(StockSearch) 생성

--- src/main/java/local/kyungmin_wms/dto/StockSearch.java
+++ src/main/java/local/kyungmin_wms/dto/StockSearch.java
@@ -1,11 +1,19 @@
 package local.kyungmin_wms.dto;
 
+import java.time.LocalDateTime;
 import local.kyungmin_wms.domain.ProductType;
 import local.kyungmin_wms.domain.StoreTemperature;
 import lombok.Data;
 
 @Data
 public class StockSearch {
+  private String stockCode;
   private StoreTemperature storeTemperature; //보관 온도(상온 , 냉동 , 냉장) 별 조회
-  private ProductType productType; //종류()
+  private ProductType productType; //상품 종류(마약 , 폭발물 , 생물학적제제 , 일반)
+  private String productName;
+  private String companyName;
+  private LocalDateTime regDate;
+  private LocalDateTime modDate;
+  private Integer quantity;
+
 }
