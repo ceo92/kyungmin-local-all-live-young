6f7b0b3da0c87ee6bdc19bddb94a16811e7d8a42
ceo92 <king9243@naver.com>
2024-09-25 17:45:04 +0900

    feat: 보관 온도 , 상품 종류 enum 생성

---
+++ src/main/java/local/kyungmin_wms/domain/ProductType.java
@@ -0,0 +1,16 @@
+package local.kyungmin_wms.domain;
+
+public enum ProductType {
+  DRUG("마약 및 항정신") , EXPLOSIVE("인화성 폭발물") , BIOLOGICAL("생물학적제제") , NORMAL("일반");
+  private final String description;
+
+  ProductType(String description){
+    this.description=description;
+  }
+
+  public String getDescription(){
+    return description;
+  }
+
+
+}
--- src/main/java/local/kyungmin_wms/dto/StockSearch.java
+++ src/main/java/local/kyungmin_wms/dto/StockSearch.java
@@ -1,5 +1,11 @@
 package local.kyungmin_wms.dto;
 
-public class StockSearch {
+import local.kyungmin_wms.domain.ProductType;
+import local.kyungmin_wms.domain.StoreTemperature;
+import lombok.Data;
 
+@Data
+public class StockSearch {
+  private StoreTemperature storeTemperature; //보관 온도(상온 , 냉동 , 냉장) 별 조회
+  private ProductType productType; //종류()
 }
