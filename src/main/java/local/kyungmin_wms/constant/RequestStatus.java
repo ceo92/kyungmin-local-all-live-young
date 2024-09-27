02d9bcbe8f90c049e2eb887a85a1e60d54ddcf24
ceo92 <king9243@naver.com>
2024-09-27 15:11:41 +0900

    refactor: RequestStatus 검수 상태 제거

--- src/main/java/local/kyungmin_wms/constant/RequestStatus.java
+++ src/main/java/local/kyungmin_wms/constant/RequestStatus.java
@@ -1,7 +1,7 @@
 package local.kyungmin_wms.constant;
 
 public enum RequestStatus {
-  WAITING_REQUEST("요청 대기") , REJECT_REQUEST("요청 반려") , INSPECTING("검수중") , INSPECT_COMPLETE("검수 완료") , INSPECT_FAIL("검수 불합");
+  WAITING_REQUEST("요청 대기") , REJECT_REQUEST("요청 반려") , INSPECT_COMPLETE("완료") , ;
   private final String description;
 
   RequestStatus(String description){
