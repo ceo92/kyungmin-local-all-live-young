4aa1ca9bbcc8bfb2ebf9de7f2b50f04592efbddf
ceo92 <king9243@naver.com>
2024-09-30 22:31:14 +0900

    refactor: findAll WarehouseMapper.xml 코드 리팩토링(resultMap 지정)

--- src/main/java/local/kyungmin_wms/dto/WarehouseSearch.java
+++ src/main/java/local/kyungmin_wms/dto/WarehouseSearch.java
@@ -2,7 +2,9 @@ package local.kyungmin_wms.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor @NoArgsConstructor
