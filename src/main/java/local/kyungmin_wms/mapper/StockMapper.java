c84b0f7d5658232b652825c0a8d7527a33b675c0
ceo92 <king9243@naver.com>
2024-10-03 18:00:59 +0900

    feat: StockMapper 페이징 처리 메서드 추가

--- src/main/java/local/kyungmin_wms/mapper/StockMapper.java
+++ src/main/java/local/kyungmin_wms/mapper/StockMapper.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import local.kyungmin_wms.domain.InboundRequestProduct;
 import local.kyungmin_wms.domain.Member;
 import local.kyungmin_wms.domain.Stock;
+import local.kyungmin_wms.dto.PageSearchDto;
 import local.kyungmin_wms.dto.StockSearch;
 import local.kyungmin_wms.dto.StockUpdateDto;
 import org.apache.ibatis.annotations.Mapper;
@@ -15,7 +16,12 @@ public interface StockMapper {
 
   void update(@Param("stockId") Long stockId , @Param("inboundRequestProduct") InboundRequestProduct inboundRequestProduct); //재고 수정(재고 실사)
 
-  List<Stock> findAll(@Param(value = "stockSearch") StockSearch stockSearch, @Param(value = "member") Member member); // 검색조건 별 전체 재고 화면 조회
+  List<Stock> findAll(@Param(value = "stockSearch") StockSearch stockSearch, @Param(value = "member") Member member, @Param("pageSearchDto")
+      PageSearchDto pageSearchDto); // 검색조건 별 전체 재고 화면 조회
+
+
+  https://congsong.tistory.com/26
+  int count(PageSearchDto pageSearchDto);
 
   Optional<Stock> findById(Long id); //상세 재고 화면 조회
 
